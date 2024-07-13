8a8d57a440fff60553e74bd84f8bee3b65e50741
Deadfubs <deadfubs@gmail.com>
2024-07-13 20:53:31 -0300

    inital page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,15 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
 
-export default function Home() {
+const MarketingPage = () => {
   return (
-    <div>
-      <Button className="bg-emerald-500 text-white">Delete</Button>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading/>
+        <Heroes/>
+      </div>
     </div>
   );
-}
+};
+
+export default MarketingPage;
