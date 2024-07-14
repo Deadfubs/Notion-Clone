ca82c6a9f78cb75d3b57728e829a5e07673b5d1f
Deadfubs <deadfubs@gmail.com>
2024-07-14 11:27:58 -0300

    landing page

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -7,15 +7,15 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas, Documents & Plans Unified. Welcome to{" "}
+        Suas Ideias, Documentos e Planos Unificados. Bem-vindo ao{" "}
         <span className="underline">Jotion</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Jotion is the connected workspace where <br /> better, faster work
-        happens.
+        Jotion é o espaço interconectado onde <br /> um trabalho mais rápido e
+        prático acontece.
       </h3>
       <Button>
-        Enter Jotion
+        Entrar
         <ArrowRight />
       </Button>
     </div>
